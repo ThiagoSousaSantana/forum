6933d9a532b0e66b94736081190b7c7a72c5cd53
Thiago Sousa <thiago91098@gmail.com>
2019-07-01 19:18:34 -0300

    Initial commit

---
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -0,0 +1,43 @@
+package br.com.alura.forum.controller;
+
+import br.com.alura.forum.controller.dto.TopicoDto;
+import br.com.alura.forum.controller.dto.TopicoForm;
+import br.com.alura.forum.repository.CursoRepository;
+import br.com.alura.forum.repository.TopicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/topicos")
+public class TopicosController {
+
+    @Autowired
+    private TopicoRepository repository;
+
+    @Autowired
+    private CursoRepository cursoRepository;
+
+    @GetMapping
+    public List list(String nomeCurso){
+        List topicos;
+        System.out.println(nomeCurso);
+        if (nomeCurso != null) topicos = repository.findByCursoNome(nomeCurso);
+        else topicos = repository.findAll();
+        return TopicoDto.convert(topicos);
+    }
+
+    @PostMapping
+    public ResponseEntity<TopicoDto> insert(@RequestBody @Valid TopicoForm form,
+                                            UriComponentsBuilder uriBuilder){
+        var topico = form.convert(cursoRepository);
+        repository.save(topico);
+
+        var uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
+        return ResponseEntity.created(uri).body(new TopicoDto(topico));
+    }
+}
---
+++ src/main/java/br/com/alura/forum/controller/dto/TopicoDto.java
@@ -0,0 +1,49 @@
+package br.com.alura.forum.controller.dto;
+
+import br.com.alura.forum.model.StatusTopico;
+import br.com.alura.forum.model.Topico;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class TopicoDto {
+
+    private Long id;
+    private String titulo;
+    private String mensagem;
+    private LocalDateTime dataCriacao;
+    private StatusTopico status;
+
+    public TopicoDto(Topico topico) {
+        this.id = topico.getId();
+        this.titulo = topico.getTitulo();
+        this.mensagem = topico.getMensagem();
+        this.dataCriacao = topico.getDataCriacao();
+        this.status = topico.getStatus();
+    }
+
+    public static List<TopicoDto> convert(List<Topico> topicos) {
+        return topicos.stream().map(TopicoDto::new).collect(Collectors.toList());
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public LocalDateTime getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public StatusTopico getStatus() {
+        return status;
+    }
+}
